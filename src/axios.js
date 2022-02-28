d86929b87e7f78304a2202ce2d36d56895e27185
Ting-Ting Chen <hchunyu.tty@gmail.com>
2022-02-28 21:08:19 +0000

    Minor CSS changes (hover behaviour)

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:5001/clone-d59c6/us-central1/api"//The Api (cloud function) URL
+    baseURL: "https://us-central1-clone-d59c6.cloudfunctions.net/api" //The Api (cloud function) URL
 });
+//baseURL: "http://localhost:5001/clone-d59c6/us-central1/api"
 export default instance;
--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: /*Info removed */,
-  authDomain: /*Info removed */,
-  projectId: /*Info removed */,
-  storageBucket: /*Info removed */,
-  messagingSenderId: /*Info removed */,
-  appId: /*Info removed */,
-  measurementId: /*Info removed */
+  apiKey: "AIzaSyANhJjLxHcoTu4ecMNdx7Dkaec0nkDUsbA",
+  authDomain: "clone-d59c6.firebaseapp.com",
+  projectId: "clone-d59c6",
+  storageBucket: "clone-d59c6.appspot.com",
+  messagingSenderId: "41930468501",
+  appId: "1:41930468501:web:78f7913b0f54c3d0093f9c",
+  measurementId: "G-6CSDPQSPMH"
 };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
